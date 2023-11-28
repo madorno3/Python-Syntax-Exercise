fcfde2e8562053d3ea6ae4976b50528f1310ecea
madorno3 <110795406+madorno3@users.noreply.github.com>
2023-11-27 22:23:18 -0500

    Update words.py

--- words.py
+++ words.py
@@ -4,7 +4,7 @@ def print_upper_words(words):
         print(word.upper())
 
 def starts_with(words):
-    """will print words in capital letters. enter words in array"""
+    
     for word in words:
         if word.startswith("e") or word.startswith("E"):
             print(word)
